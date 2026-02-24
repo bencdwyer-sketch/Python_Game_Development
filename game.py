ff0a7fb678bd1f942745584fb7029f342140514c
bencdwyer-sketch <bencdwyer@gmail.com>
2026-02-24 18:27:28 +0000

    Add files via upload

---
+++ game.py
@@ -0,0 +1,39 @@
+# import pgzrun
+# import random
+
+# TITLE = 'Good Shot'
+# WIDTH = 500
+# HEIGHT = 500
+# message =""
+# alien = Actor('alien')
+
+# def draw():
+#     screen.clear()
+#     screen.fill(154,57,215)
+#     alien.draw()
+#     screen.draw.text(message,topleft=(400,10),fontsize=30)
+
+# def place_alien()
+#     alien.x = random.randint(50,500)
+#     alien.y = random.randint()
+
+# def on_mouse_down(pos):
+#     global message
+#     if alien.collidepoint(pos):
+#         message = 'Good Shot'
+#         place_alien()
+#     else:
+#         message = 'You Missed'
+
+# place_alien()
+# pgzrun.go()
+
+
+
+
+
+
+
+
+
+
